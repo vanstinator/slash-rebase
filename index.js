d07135b0e4902ebf6d449ea4ea558f44658fdbba
Glitch (probot-hello-world) <none>
2020-04-17 17:47:11 +0000

    🍢👣 Checkpoint ./app.yml:11201561/27 ./index.js:11201561/3392

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-module.exports = (app) => {
-  // Your code here
-  app.log('Yay! The app was loaded!')
+const commands = require('probot-commands')
 
-  // example of probot responding 'Hello World' to a new issue being opened
-  app.on('issues.opened', async context => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({body: 'Hello World!'})
+module.exports = app => {
+  console.log('App loaded')
+  // Type `/label foo, bar` in a comment box for an Issue or Pull Request
+  commands(app, 'rebase', (context, command) => {
+    // const labels = command.arguments.split(/, */)
+    console.log(context)
+    const params = context.issue({body: 'Rebased this branch successfully!'})
 
     // Post a comment on the issue
     return context.github.issues.createComment(params)
