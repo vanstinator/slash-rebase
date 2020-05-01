5dc24081329be97df7f293a244974b26a0273faf
Glitch (probot-hello-world) <none>
2020-05-01 15:48:47 +0000

    👲👏 Checkpoint ./app.yml:15729404/16 ./index.js:15729404/1240

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const commands = require('probot-commands')
+const commands = require("probot-commands");
 
 module.exports = app => {
-  console.log('App loaded')
+  console.log("App loaded");
   // Type `/label foo, bar` in a comment box for an Issue or Pull Request
-  commands(app, 'rebase', (context, command) => {
+  commands(app, "rebase", (context, command) => {
     // const labels = command.arguments.split(/, */)
-    const params = context.issue({body: 'Rebased this branch successfully!'})
-
+    const params = context.issue({ body: "Rebased this branch successfully!" });
+    const git_token = context.actions.getSecret({
+      'plexinc',
+      repo,
+      name
+    });
     // Post a comment on the issue
-    return context.github.issues.createComment(params)
-  })
-}
+    return context.github.issues.createComment(params);
+  });
+};
