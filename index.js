82cb125c6753c879344dfdc5a5782461fc6e67aa
Glitch (probot-hello-world) <none>
2020-05-01 16:00:43 +0000

    ⛱🍘 Checkpoint ./index.js:15729404/3721

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const commands = require("probot-commands");
 module.exports = app => {
   console.log("App loaded");
   // Type `/label foo, bar` in a comment box for an Issue or Pull Request
-  commands(app, "rebase", (context, command) => {
+  commands(app, "rebase", async (context, command) => {
     // const labels = command.arguments.split(/, */)
     const params = context.issue({ body: "Rebased this branch successfully!" });
-    const git_token = context.actions.getSecret({
-      'plexinc',
-      repo,
-      name
+    
+    console.log(context.github)
+    
+    const git_token = await context.github.actions.getSecret({
+      owner: "vanstinator",
+      repo: "bot-test",
+      name: "git-token"
     });
     // Post a comment on the issue
     return context.github.issues.createComment(params);
