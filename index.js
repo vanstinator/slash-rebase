fe556a6e2ae16182bc53eab44f8dbbd033d6d1b7
Glitch (probot-hello-world) <none>
2020-05-04 22:27:06 +0000

    🕶🐫 Checkpoint ./index.js:15729404/1298

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const git = require("simple-git/promise");
 const USER = "vanstinator";
 const PASS = process.env.GITHUB_TOKEN;
 const REPO = "github.com/vanstinator/bot-test";
+const ROOT_BRANCH = 'master';
 
 const remote = `https://${USER}:${PASS}@${REPO}`;
 
@@ -17,25 +18,31 @@ module.exports = app => {
     // There's some breaking octokit changes in the pipe. Will be resolved in probot v10 
     const comment = { ...params, issue_number: params.number };
     delete comment.number;
-    
+       
     // clone the repo if it doesn't already exist
     try {
-      await git()
-        .silent(true)
-        .clone(remote);
+      await git().clone(remote);
     } catch (e) {
-      console.log(`${REPO} already checked out`, e);
+      console.log(`${REPO} already checked out`);
     }
     
-    // force delete local develop and pull from remote
+    // try to pull with rebase local develop. if it fails delete local and pull again
+    try {
+      await git().checkout(ROOT_BRANCH);
+    } catch (e) {
+      console.log('develop pull failed. delete and try again');
+      await git().deleteLocalBranch(ROOT_BRANCH, true);
+      await git().checkout(ROOT_BRANCH);
+    }
     
     // checkout the PR branch
+    console.log(JSON.stringify(context.payload, null, 2))
     
     // rebase on develop
     
     // force push
     
-    // checkout develop and delete local 
+    // checkout develop and delete local pr branch
 
     // Post a comment on the issue
     return context.github.issues.createComment(comment);
