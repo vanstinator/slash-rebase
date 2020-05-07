4d0c22cc06a458822bfdd47dbb991d68f6d97bf2
Justin Vanderhooft <justinvdhooft@gmail.com>
2020-05-07 10:11:33 -0400

    fix: check for hash match much more reliably

--- bin/rebase.sh
+++ bin/rebase.sh
@@ -29,23 +29,28 @@ cd $repo
 git config user.name $name
 git config user.email $email
 
+git checkout $base
+git checkout $head
+
 # First lets make sure we even need to do anything
 hash1=$(git show-ref --heads -s $base)
 hash2=$(git merge-base $base $head)
-[ "${hash1}" = "${hash2}" ] && exit 1 || echo "Rebase is required"
 
-git checkout $base
-# Checkout and pull head
-git checkout $head
+if [ "${hash1}" != "${hash2}" ] ; then
+  echo "Rebase is required"
 
-# Do the rebase
-if git rebase $base ; then
-    echo "Rebase Succeeded. Force pushing"
-    git push -f origin
-    exit 0
+  # Do the rebase
+  if git rebase $base ; then
+      echo "Rebase Succeeded. Force pushing"
+      git push -f origin
+      exitCode=0
+  else
+      echo "Rebase Failed. Cleaning up"
+      git rebase --abort
+      exitCode=2
+  fi
 else
-    echo "Rebase Failed. Cleaning up"
-    git rebase --abort
+  exitCode=1
 fi
 
 # Clean up
@@ -53,4 +58,4 @@ cd ..
 rm -r $repo
 
 # Explictly end with nothing to do
-exit 2
+exit $exitCode
